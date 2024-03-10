84ac0a6eb8c8fe9da0cf5dfc4f8a38bc13c58f3a
주재민 <als982001@naver.com>
2024-03-10 13:19:15 +0900

    데이터바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1 :style="titleStyle">원룸보여줌</h1>
+  <div>
+    <h4>{{ products[0] }} 원룸</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }} 원룸</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+  },
+  data() {
+    return {
+      price1: 60,
+      price2: 50,
+      titleStyle: "color: blue",
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+    };
+  },
+};
 </script>
 
 <style>
