2560fdae7ab41130ebd95302bc002b02c1114010
주재민 <als982001@naver.com>
2024-03-10 13:31:24 +0900

    반복문(v-for) 적용

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,12 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <h1 :style="titleStyle">원룸보여줌</h1>
-  <div>
-    <h4>{{ products[0] }} 원룸</h4>
-    <p>{{ price1 }} 만원</p>
+  <div class="menu">
+    <a v-for="(item, index) in menu" :key="item">{{ `${index}. ${item}` }}</a>
   </div>
-  <div>
-    <h4>{{ products[1] }} 원룸</h4>
-    <p>{{ price2 }} 만원</p>
+  <div v-for="(product, index) in products" :key="product">
+    <h4>{{ product }}</h4>
+    <p>{{ prices[index] + " 만원" }}</p>
   </div>
 </template>
 
@@ -22,10 +21,10 @@ export default {
   },
   data() {
     return {
-      price1: 60,
-      price2: 50,
       titleStyle: "color: blue",
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      prices: [50, 60, 70],
+      menu: ["Home", "Products", "About"],
     };
   },
 };
@@ -39,5 +38,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
